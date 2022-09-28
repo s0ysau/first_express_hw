658601e5607e3cdebad062a56ec0a083409f075f
jerricklc@gmail.com <jerricklc@gmail.com>
2022-09-28 13:20:07 -0400

    Tip calculator Express application created

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const app = express();
 
 
 // Mount routes
+// ==== Greeting Express ==== //
 app.get('/greeting', (req, res) => {
     res.send('<h1>Hello strangers</h1>');
 });
@@ -18,6 +19,15 @@ app.get('/:name' , (req, res) => {
     const name = req.params.name
     res.send(`<h1>What\'s up, ${name}</h1>`)
 })
+// ==== Greeting Express ==== //
+// ==== Tip Calculator Express ==== //
+app.get('/tip/:total/:tipPercentage', (req, res) => {
+    const tipTotal = parseInt(req.params.total) * (parseInt(req.params.tipPercentage))/100
+    res.send(`The amount is \$ ${tipTotal}`)
+})
+
+
+// ==== Tip Calculator Express ==== //
 
 
 // Tell the app to listen on port 3000
