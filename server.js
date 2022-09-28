b929fca7e432a531637e6f108a98de7ed970a71d
jerricklc@gmail.com <jerricklc@gmail.com>
2022-09-28 14:43:35 -0400

    Magic 8 Ball express application created

--- server.js
+++ server.js
@@ -26,10 +26,16 @@ app.get('/tip/:total/:tipPercentage', (req, res) => {
     res.send(`The amount is \$ ${tipTotal}`)
 })
 
-
 // ==== Tip Calculator Express ==== //
+// ==== Magic 8 ball Express ==== //
+
+app.get('/magic/:question', (req, res) => {
+    const responses = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely","You may rely on it", "As I see it yes", "Most likely", "Outlook good","Yes", "Signs point to yes", "Reply hazy try again", "Ask again later","Better not tell you now", "Cannot predict now", "Concentrate and ask again","Don't count on it", "My reply is no", "My sources say no","Outlook not so good", "Very doubtful"]
+    res.send(`<h1>${responses[Math.floor(Math.random() * 20)]}</h1>`)
+})
 
 
+// ==== Magic 8 ball Express ==== //
 // Tell the app to listen on port 3000
 app.listen(3000, function() {
     console.log('Listening on port 3000');
